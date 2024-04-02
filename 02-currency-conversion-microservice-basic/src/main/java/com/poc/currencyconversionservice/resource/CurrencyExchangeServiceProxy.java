ff57b6c7840e5d26e1bf97476776f0e35dda2863
it <muralip.wk@gmail.com>
2024-04-02 16:51:53 +0100

    xdx

---
+++ 02-currency-conversion-microservice-basic/src/main/java/com/poc/currencyconversionservice/resource/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,13 @@
+package com.poc.currencyconversionservice.resource;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")//KUB_HARD_CODED: Works
+//@FeignClient(name = "currency-exchange", url = "${KEY_CURRENCY_EXCHANGE_SERVICE_HOST:XXX}:${KEY_CURRENCY_EXCHANGE_SERVICE_PORT:7777}") //KEY_CURRENCY_EXCHANGE_SERVICE_HOST in config map works
+public interface CurrencyExchangeServiceProxy {
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from,
+			@PathVariable("to") String to);
+}
